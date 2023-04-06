a2cd694347523e3cafbbde607454582917d9bc4b
clarissavlsqz <clarissa.a.velasquez@gmail.com>
2023-04-06 17:13:25 -0600

    Initial Commit

---
+++ TaylorSwiftGuess/TaylorSwiftGuessApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaylorSwiftGuessApp.swift
+//  TaylorSwiftGuess
+//
+//  Created by Clarissa Velásquez Magaña on 06/04/23.
+//
+
+import SwiftUI
+
+@main
+struct TaylorSwiftGuessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
