786a4ca6d3ae8562a532e8afe3c512a202aed207
clarissavlsqz <clarissa.a.velasquez@gmail.com>
2023-04-07 14:48:22 -0600

    create home screen

--- TaylorSwiftGuess/ContentView.swift
+++ TaylorSwiftGuess/ContentView.swift
@@ -9,13 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("taylor_home")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+            
+            Rectangle()
+                    .foregroundColor(.clear)
+                    .background(LinearGradient(gradient: Gradient(colors: [.clear, Color(red: 0.96, green: 0.74, blue: 0.86)]), startPoint: .center, endPoint: .bottom))
+            
+            ZStack {
+                VStack {
+                    Text("Guess the...")
+                        .font (
+                            .system (
+                                .title
+                            )
+                            .weight(.light)
+                        )
+                        .italic()
+                        .foregroundColor(Color(red: 1.00, green: 0.95, blue: 0.39))
+                        .padding(.bottom, 15)
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Song")
+                            .frame(maxWidth: 180)
+                            .fontWeight(.semibold)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .cornerRadius(50)
+                    .tint(Color(red: 0.72, green: 0.24, blue: 0.42))
+                    .padding(15)
+                    
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Album")
+                            .frame(maxWidth: 180)
+                            .fontWeight(.semibold)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .cornerRadius(50)
+                    .tint(Color(red: 0.44, green: 0.58, blue: 0.71))
+                        
+                }
+                .padding(.top, 440)
+            }
+            
         }
-        .padding()
     }
 }
 
